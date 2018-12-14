906bb268d63c1b677cd96e337f78416eb5ded7a8
pd1714git <44477383+pd1714git@users.noreply.github.com>
2018-12-14 17:51:26 +0100

    Add files via upload

--- logement_app.R
+++ logement_app.R
@@ -30,35 +30,19 @@ ui <- fluidPage(
                  helpText("Time for the fun part. Enjoy a night out on the town, having the best options for your kids future or living in a quiet environment, then customise your search to find the flat that is made for you. In the following section, check the boxes if you are interested in a specific criteria, if you not by default the algorithm will not take them into account."),
                  
                  h3("Transport links"),
-                 radioButtons(inputId="metromaybe", label="Do you need to be within a 500 m walk to a tube station?", choices=c("Yes"=1,"Not interested"=0), selected = 0, inline = T),
-                 conditionalPanel(condition = "input.metromaybe == 1",
-                                  wellPanel(
-                                    sliderInput(inputId = "metro",label = "Select the level of access to the tube that you want, 1 being the lowest service in Paris and 10 the higest",value = summary(tri_categories$stations_qt)[c(1,6)],summary(tri_categories$stations_qt)[1],max= summary(tri_categories$stations_qt)[6])
-                                  ) ),
+                 sliderInput(inputId = "metro",label = "What is the level of public transport you require, 1 being the highest level of public transport within 500m from your flat in Paris, and 10 being the lowest.",value = summary(tri_categories$stations_qt)[c(1,6)],summary(tri_categories$stations_qt)[1],max= summary(tri_categories$stations_qt)[6]),
                  
                  h3("Daily Life"),
-                 radioButtons(inputId = "schoolmaybe",label = "Do you need to be within a 500 m walk to a tube station?", choices=c("Yes"=1,"Not interested"=0), selected = 0, inline = T),
-                 conditionalPanel(condition = "input.schoolmaybe == 1",
-                                  wellPanel(
-                                    sliderInput(inputId = "school",label = "Select the level of school density that you want, 1 being the lowest density in Paris and 10 being the highest",value = summary(tri_categories$schools_qt)[c(1,6)],summary(tri_categories$schools_qt)[1],max= summary(tri_categories$schools_qt)[6])
-                                  ) ),
-                 radioButtons(inputId = "commercemaybe",label = "Enjoy having everything close to you? Select yes if you want to be near bakeries, supermarkets, butchers, etc...", choices=c("Yes"=1,"Not interested"=0), selected = 0, inline = T),
-                 conditionalPanel(condition = "input.commercemaybe == 1",
-                                  wellPanel(
-                                    sliderInput(inputId = "commerce",label = "Select the density of shops you need, 1 being a neighbourhood with few options and 10 with a lot",value = summary(tri_categories$commerce_qt)[c(1,6)],summary(tri_categories$commerce_qt)[1],max= summary(tri_categories$commerce_qt)[6])
-                                  ) ),
+                 sliderInput(inputId = "school",label = "Picky chooser? Select the level of school density you want near your flat, 1 being the highest density of schools within 500m from your flat in Paris, and 10 being the lowest.",value = summary(tri_categories$schools_qt)[c(1,6)],summary(tri_categories$schools_qt)[1],max= summary(tri_categories$schools_qt)[6]),
+                 sliderInput(inputId = "commerce",label = "Bit of a shopaholic? Select the level of shop density (bakery, butcher, supermarket, etc..) you want near your flat, 1 being the highest density of shops within 500m from your flat in Paris, and 10 being the lowest.",value = summary(tri_categories$commerce_qt)[c(1,6)],summary(tri_categories$commerce_qt)[1],max= summary(tri_categories$commerce_qt)[6]),
                  
                  h3("Entertainment"),
-                 radioButtons(inputId = "restomaybe",label = "Fancy going out a lot?", choices=c("Yes"=1,"Not interested"=0), selected = 0, inline = T),
-                 conditionalPanel(condition = "input.restomaybe == 1",
-                                  wellPanel(
-                                    sliderInput(inputId = "resto",label = "Select the level of activity you want, 1 being a very quite neighbourhood and 10 being a lively one",value = summary(tri_categories$anime_resto_cine_qt)[c(1,6)],summary(tri_categories$anime_resto_cine_qt)[1],max= summary(tri_categories$anime_resto_cine_qt)[6])
-                                  ) )
-                 
+                 sliderInput(inputId = "resto",label = "Fancy going out a lot? Select the level of activity you want, 1 being a very busy neighbourhood and 10 being a quiet one",value = summary(tri_categories$anime_resto_cine_qt)[c(1,6)],summary(tri_categories$anime_resto_cine_qt)[1],max= summary(tri_categories$anime_resto_cine_qt)[6])
     ),
     
     mainPanel(
-      fluidPage(google_mapOutput("map"), DT::dataTableOutput('table1'))
+      fluidPage(google_mapOutput("map")), 
+      fluidPage(DT:::dataTableOutput('table1', width = "50%", height = "10%"))
     )
     
   )
@@ -66,7 +50,7 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-  castorus_table <- castorus_table %>% dplyr::select(-X1) %>% rename("X" = X1_1, "Change_price" = c(11)) 
+  castorus_table <- castorus_table %>% dplyr::select(-X1) %>% rename("X" = X1_1, "Change_price" = c(11)) %>%  dplyr::select(-depuis,-Change_price,lextrait.n)
   tri_categories <- tri_categories %>% dplyr::select(-X) %>%  rename("X" = apartment_index) 
   castorus_table <- castorus_table %>% mutate(list_url_description = glue::glue("<a href='{list_url_description}'>{list_url_description} </a>"))
   # castorus_table <- as.data.frame(castorus_table)
@@ -89,10 +73,6 @@ server <- function(input, output) {
   surface2 <- reactive(max(input$surface))
   rooms1 <- reactive(min(input$rooms))
   rooms2 <- reactive(max(input$rooms))
-  metroyes <- reactive(input$metromaybe)
-  schoolyes <- reactive(input$schoolmaybe)
-  commerceyes <- reactive(input$commercemaybe)
-  restoyes <- reactive(input$restomaybe)
   N1 <- reactive(input$name)
   castorus_table_fixed <- reactive(castorus_table)
   
@@ -102,73 +82,52 @@ server <- function(input, output) {
   iconUrl <- paste0("https://developers.google.com/maps/documentation/",
                     "javascript/examples/full/images/beachflag.png")
   
-  #' Title
+  #' The Function inputs a dataframe and filters out the quantiles for a specific category that the user wants or do not want.
   #'
-  #' @param castorus_table k
-  #' @param tri 
-  #' @param min 
-  #' @param max 
-  #' @param value 
+  #' @param castorus_table a data frame we are going to modify
+  #' @param tri The dataframe with the scores
+  #' @param min The miminum quantile we want.
+  #' @param max The highest quantile we want.
+  #' @param value Select the column in tri which is going to be filtered.
   #'
-  #' @return
+  #' @return a data frame
   #' @export
   #'
-  #' @examples
   filter_slider <- function(castorus_table, tri, min, max, value){
     temp<- tri %>% dplyr::filter(tri_categories[,value] %in% c(min:max)) %>% select(X) 
     a <- castorus_table %>% dplyr::filter(X %in% temp$X)
     return(a)
   }
   
-  a_metro <- reactive({
-    a1 <- if (metroyes() == 1){
-      metro1 <- reactive(min(input$metro))
-      metro2 <- reactive(max(input$metro))
-      castorus_table2 <- filter_slider(castorus_table1(),tri_categories, metro1(), metro2(), 8)
-    } else {
-      castorus_table2 <- castorus_table1()
-    }
-    a_metro <- left_join(castorus_table2,castorus_table1())
-  })
+  metro1 <- reactive(min(input$metro))
+  metro2 <- reactive(max(input$metro))
+  castorus_table2 <- reactive(filter_slider(castorus_table1(),tri_categories, metro1(), metro2(), 8))
   
-  a_school <- reactive({
-    a2 <- if (schoolyes() == 1){
-      school1 <- reactive(min(input$school))
-      school2 <- reactive(max(input$school))
-      castorus_table3 <- filter_slider(castorus_table1(),tri_categories, school1(), school2(), 7)
-    } else {
-      castorus_table3 <- a_metro()
-    }
-    a2 <-left_join(castorus_table3,a_metro())
-  })
+  school1 <- reactive(min(input$school))
+  school2 <- reactive(max(input$school))
+  castorus_table3<- reactive(filter_slider(castorus_table2(),tri_categories, school1(), school2(), 7))
   
-  a_commerce <- reactive({
-    a3 <- if (commerceyes() == 1){
-      commerce1 <- reactive(min(input$commerce))
-      commerce2 <- reactive(max(input$commerce))
-      castorus_table4 <- filter_slider(castorus_table1(),tri_categories, commerce1(), commerce2(), 6)
-    } else {
-      castorus_table4 <- a_school()
-    }
-    a3 <- left_join(castorus_table4,a_school())
-  })  
+  commerce1 <- reactive(min(input$commerce))
+  commerce2 <- reactive(max(input$commerce))
+  castorus_table4 <- reactive(filter_slider(castorus_table3(),tri_categories, commerce1(), commerce2(), 6))
   
-  a_resto <- reactive({
-    a4 <- if (restoyes() == 1){
-      resto1 <- reactive(min(input$resto))
-      resto2 <- reactive(max(input$resto))
-      castorus_table5 <- filter_slider(a_commerce(),tri_categories, resto1(), resto2(), 9)
-    } else {
-      castorus_table5 <- a_commerce()
-    }
-    a4 <- left_join(castorus_table5,a_metro())
-  })  
+  resto1 <- reactive(min(input$resto))
+  resto2 <- reactive(max(input$resto))
+  castorus_table5 <- reactive(filter_slider(castorus_table4(),tri_categories, resto1(), resto2(), 9))
   
-  df2 <- reactive(a_resto())
+  df2 <- reactive(castorus_table5())
   df1 <- reactive(coordinates(df2()))
   
-  output$table1 <- DT::renderDataTable({DT::datatable(df2())}, escape = FALSE,options=list(stateSave = TRUE))
+  output$table1 <- DT::renderDataTable({df2()}, escape = FALSE)
   
+#' The function takes a dataframe and inputs a new column for the marker image, and also selects the longitude and lattitude column.
+#'
+#' @param castorus_table work on this data frame to obtain the longitude, the lattitude and icon shape colum. 
+#'
+#' @return a data frame with longitude lattitude and the icon shape column.
+#' @export
+#'
+#' @examples
   coordinates <- function(castorus_table){
     a <- castorus_table %>% dplyr::select("latitude","longitude") %>% dplyr::mutate(icon = iconUrl)
     return(a)
@@ -176,24 +135,21 @@ server <- function(input, output) {
   
   api_key <- reactive("AIzaSyDBIYQwxvUVTn5GumtMtMZhXZtbyw7KPrM")
   
+#' This function is used to be able to obtain the indexes of the selected rows in the datatable.
+#'
+#' @param c the indexes of the selected rows
+#' @param df1 the dataframe which is going to be used.
+#'
+#' @return a data frame.
+#' @export
+#'
+#' @examples
   column_index <- function(c, df1){
     df1 <- df1[c,]
   }
   
-  bb <- reactive({input$table1_rows_selected})
-  a <-  reactive(column_index(input$table1_rows_selected,coordinates(df2())))
   output$map <- renderGoogle_map({
-    google_map(key = api_key(),  data = column_index(input$table1_rows_selected,coordinates(df2())), location = c(48.87515, 2.339354), zoom =  11) %>% add_markers(lat = "latitude", lon = "longitude", marker_icon = "icon")
-  })
-  
-  
-  
-  output$x4 = renderPrint({
-    s = input$table1_rows_selected
-    if (length(s)) {
-      cat('These rows were selected:\n\n')
-      cat(s, sep = ', ')
-    }
+    google_map(key = api_key(),  data = column_index(input$table1_rows_selected,coordinates(df2())), location = c(48.87515, 2.339354), zoom = 11) %>% add_markers(lat = "latitude", lon = "longitude", marker_icon = "icon") 
   })
   
 }
